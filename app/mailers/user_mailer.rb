45731247a104d69a1a96f554aed689bf4564bf4d
DaichiA <sss.fbs7.fre.e@gmail.com>
2020-11-29 17:43:06 +0900

    Update reset mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -27,7 +27,7 @@ class UserMailer < ApplicationMailer
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
     mail(:to => user.email,
-       :subject => "Your password has been reset")
+       :subject => "COFFEE ANYWHERE パスワードリセット")
   end
 
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe UserMailer, type: :mailer do
   describe "activation_needed_email" do
     let(:mail) { UserMailer.activation_needed_email(user) }
     let(:user) { FactoryBot.create(:user)}
+    let(:reset_mail){ UserMailer.reset_password_email(user) } 
 
     it "renders the headers" do
       # expect(mail.subject).to eq('Welcome to Coffee Anywhere!')
@@ -19,6 +20,24 @@ RSpec.describe UserMailer, type: :mailer do
       # expect(mail.body.encoded).to match (@url)
       
     end
+
+    # describe "password reset mail" do
+    #   it "renders the headers" do
+    #     # expect(mail.subject).to eq('Welcome to Coffee Anywhere!')
+    #     expect(reset_mail.subject).to eq('COFFEE ANYWHERE パスワードリセット')
+    #     expect(reset_mail.to).to eq([user.email])
+    #     expect(reset_mail.from).to eq(["from@example.com"])
+    #   end
+  
+    #   it "renders the body" do
+    #     # expect(mail.body.encoded).to have_content (user.email)
+    #     expect(reset_mail.body.encoded).to have_content (@url)
+    #     # expect(mail.body.encoded).to match (@user.email)
+    #     # expect(mail.body.encoded).to match (@url)
+        
+    #   end
+    # end
+
   end
 
   # describe "activation_success_email" do
