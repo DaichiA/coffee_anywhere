fb3a7ce8655da527dfaccf6a71e9ce3656ee0a3c
DaichiA <sss.fbs7.fre.e@gmail.com>
2020-12-16 21:44:45 +0900

    add stylesheet_pack_tag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-// require("jquery-ui")  tag-itを使うためにgemを入れたが、Module not found: Error: Can't resolve 'jquery-ui'が出て解決できず
-import "bootstrap"
+// require("jquery")
+import "bootstrap";
 import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/js/all';
 
