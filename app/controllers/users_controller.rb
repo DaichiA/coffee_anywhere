5508b1d606954071841e6b60521e3d73f6e91669
DaichiA <sss.fbs7.fre.e@gmail.com>
2021-01-04 11:01:21 +0900

    Update user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: [:index]
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 20)
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def show
