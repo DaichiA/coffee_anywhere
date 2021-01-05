dd1c3c8f18938afb13243b87c6afa0a3db9cef2e
DaichiA <sss.fbs7.fre.e@gmail.com>
2021-01-05 13:38:38 +0900

    Update search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    # @q = Shop.ransack(params[:q])
-    # @shops = @q.result(distinct: true).paginate(page: params[:page])
+    @q = Shop.ransack(params[:q])
     @tags = Tag.all
     tag_names = @tags.pluck(:tag_name)
     brew_methods = ['ドリップ', 'エスプレッソ', 'フレンチプレス', 'エアロプレス', '水出し', 'サイフォン']
     @tags_for_search = tag_names - brew_methods
-
-    if params[:q] != nil
-      @q = params[:q].split(/[\p{blank}\s]+/)
-      grouping_hash = @q.reduce({}) { |hash, word| hash.merge(word => { title_cont: word }) }
-      @shops = Shop.ransack(
-        { combinator: 'and', groupings: grouping_hash, s: 'title desc' }
-      ).result(distinct: true).paginate(page: params[:page])
-    else
-      @q = Shop.ransack(params[:q])
-      @shops = @q.result(distinct: true).paginate(page: params[:page])
-    end
   end
 
   def terms; end
