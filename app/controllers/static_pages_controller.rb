495b0be9ec9538b625e4103fde317cf059dd71d3
DaichiA <sss.fbs7.fre.e@gmail.com>
2021-02-04 10:50:15 +0900

    Nothing has done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,10 @@ class StaticPagesController < ApplicationController
     tag_names = @tags.pluck(:tag_name)
     brew_methods = %w[ドリップ エスプレッソ フレンチプレス エアロプレス 水出し サイフォン]
     @tags_for_search = tag_names - brew_methods
+
+    # rand = Shop.pluck(:id).sample(3)
+    # @shops = Shop.find(rand)
+    # @shops.each do |@shop| end
   end
 
   def terms; end
