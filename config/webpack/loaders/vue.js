f778c010bad681e216cf77e3329eaf69f9975b8f
DaichiA <sss.fbs7.fre.e@gmail.com>
2021-02-13 10:01:35 +0900

    try to apply css of vuetify

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -2,5 +2,29 @@ module.exports = {
   test: /\.vue(\.erb)?$/,
   use: [{
     loader: 'vue-loader'
-  }]
+  }],
+  rules: [
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require('sass'),
+            indentedSyntax: true // optional
+          },
+          // Requires sass-loader@^8.0.0
+          options: {
+            implementation: require('sass'),
+            sassOptions: {
+              indentedSyntax: true // optional
+            },
+          },
+        },
+      ],
+    },
+  ],
 }
