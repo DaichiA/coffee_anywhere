5b73ebff46313ef7cbf111de5ca97accfed8ce26
DaichiA <sss.fbs7.fre.e@gmail.com>
2021-02-13 11:03:13 +0900

    try to apply css of vuetify

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import "vuetify/dist/vuetify.min.css"; // 追加
 import Vuetify from "vuetify"; // 追加
+import "vuetify/dist/vuetify.min.css"; // 追加
 
 Vue.use(Vuetify); // 追加
 const vuetify = new Vuetify(); // 追加
